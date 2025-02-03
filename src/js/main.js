939a0ca1d7489664e225fafa5add94f4c7efed33
t0mmyq <a231660al@gmail.com>
2025-02-03 13:53:52 +0200

    Add input from keyboard

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ const EQUALS = document.getElementById('equals');
 const CLEAR = document.getElementById('AC')
 let output = document.getElementById('input-log');
 const SIGNS = ["-", "+", "/", "*", "=", "."];
+const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 BUTTONS.forEach((button)=>{
     button.addEventListener('click', function(){
@@ -33,4 +34,26 @@ EQUALS.addEventListener('click', function(){
 })
 CLEAR.addEventListener('click', function(){
     output.textContent = '';
+})
+document.addEventListener('keydown', function(event){
+    if(NUMBERS.includes(event.key) || SIGNS.includes(event.key)){
+        output.textContent += event.key;
+    }
+    if(event.key === "Enter"){
+        try{
+            let exception_test = output.textContent;
+            // Comment priekš Renāra. Šeit izmantoju regex, lai uzķert / 0 except. 
+            // Vajadzēja ieguglēt, kā ar to Regex strādāt, kko sapratu, bet ar palidzību!
+            if(/\/0([^.\d]|$)/.test(exception_test)){
+                throw new Error("Cannot divide by zero!")
+            }
+            output.textContent = eval(exception_test);
+        }catch(error){
+            console.error("Invalid expression. There are some errors: ", error);
+            output.textContent = 'Error!    ';
+        }
+    }
+    if(event.key === "Backspace"){
+        output.textContent = output.textContent.slice(0, -1);
+    }
 })
